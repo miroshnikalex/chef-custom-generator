e1d5e130b4bdbe3deaa76e30d16a1b76fccb4746
Oleksandr (Alex) Miroshnyk <miroshnik.alex@gmail.com>
2017-10-19 14:23:15 +0000

    v 0.1.0 - Added and configured Docker driver for test kitchen

---
+++ metadata.rb
@@ -0,0 +1,5 @@
+name             'chef-custom-generator'
+description      'Custom code generator cookbook for use with ChefDK'
+long_description 'Custom code generator cookbook for use with ChefDK'
+version          '0.1.0'
+
